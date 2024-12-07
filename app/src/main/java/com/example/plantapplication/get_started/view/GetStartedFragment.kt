4e764d104df22d9f61e8cf2d3491f729091aa6f5
fatihbcan <rhenium96@icloud.com>
2024-12-07 11:33:49 +0300

    GetStarted UI implemented

---
+++ app/src/main/java/com/example/plantapplication/get_started/view/GetStartedFragment.kt
@@ -0,0 +1,9 @@
+package com.example.plantapplication.get_started.view
+
+
+import androidx.fragment.app.Fragment
+import com.example.plantapplication.R
+
+class GetStartedFragment : Fragment(R.layout.fragment_get_started) {
+
+}
